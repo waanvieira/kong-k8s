498e3e331ef019644152b3ca1ee3219dd3efa970
wandersonvieira <wandersonvieira@movida.com.br>
2024-03-13 21:34:21 -0300

    Observações e anotações

--- infra/kong-k8s/efk/fluentd/fluentd.sh
+++ infra/kong-k8s/efk/fluentd/fluentd.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# helm repo add fluent https://fluent.github.io/helm-charts
+# helm repo update
+# helm install fluentd fluent/fluentd --namespace=logs -f fluentd-values.yaml
+
 helm repo add fluent https://fluent.github.io/helm-charts
-helm install fluentd fluent/fluentd --namespace=logs -f fluentd-values.yaml
+helm repo update
+helm install fluentd fluent/fluentd --namespace=logs
+
